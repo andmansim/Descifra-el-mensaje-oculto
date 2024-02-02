93f4879745651e72302bbb2e804471319fd1549a
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-02 17:09:34 +0100

    dnfjdnfjdf

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 
-def abclave(original):
+def abclave(original, preguntita):
   abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   zyx = 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
   resultado = ''
+  
+  if preguntita == 'c':
+    abc, zyx = abc, zyx
+  else:
+    abc, zyx = zyx, abc
   for i in original:
     resultado += zyx[abc.index(i)]
   return resultado
 
 
+
 if __name__ == '__main__':
+  preguntita = input('Quieres cifrar o descifrar? (c/d): ')
   original = input("Introduzca la clave: ")
-  resultado = abclave(original)
+  resultado = abclave(original, preguntita)
   print(resultado)
 
 'GSVUOZTRHHZBDVZIVXIZAB'
